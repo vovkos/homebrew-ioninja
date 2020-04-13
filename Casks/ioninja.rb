558b61c1c9ae88597e645d98a9a6b90d54c5414e
Vladimir Gladkov (office-mac) <vovkos@gmail.com>
2020-04-13 18:08:36 +0800

    fix: typo in download URL

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -2,7 +2,7 @@ cask 'ioninja' do
   version '3.14.0'
   sha256 '9dd0cbfcdf91a78e007ffcd73e0dfc55f34a54ad69755a3d99b30b822851fc67'
 
-  url "https://tibbo.com/downloads/archive/ioninja/#{version}/ioninja-#{version}-macos.dmg"
+  url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac.dmg"
   name 'IO Ninja'
   homepage 'https://ioninja.com'
 
