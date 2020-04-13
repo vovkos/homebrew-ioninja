dc78cb118f91218127b50bbb4744ff3e4cd07dc4
Vladimir Gladkov (office-mac) <vovkos@gmail.com>
2020-04-13 18:02:07 +0800

    ioninja-3.14.0 (initial commit)

---
+++ Casks/ioninja.rb
@@ -0,0 +1,22 @@
+cask 'ioninja' do
+  version '3.14.0'
+  sha256 '9dd0cbfcdf91a78e007ffcd73e0dfc55f34a54ad69755a3d99b30b822851fc67'
+
+  url "https://tibbo.com/downloads/archive/ioninja/#{version}/ioninja-#{version}-macos.dmg"
+  name 'IO Ninja'
+  homepage 'https://ioninja.com'
+
+  auto_updates true
+
+  depends_on macos: '>= :sierra'
+
+  app 'IO Ninja.app'
+  binary "#{appdir}/IO Ninja.app/Contents/MacOS/ioninja"
+
+  zap trash: [
+               '~/.ioninja',
+               '~/Library/Caches/com.tibbo.ioninja',
+               '~/Library/Preferences/com.tibbo.ioninja.plist',
+               '~/Library/Saved Application State/com.tibbo.ioninja.savedState',
+             ]
+end
---
+++ README.md
@@ -0,0 +1,19 @@
+# Homebrew Tap for IO Ninja
+
+## Abstract
+
+This repo provides a Homebrew tap with the latest official Mac OSX builds of IO Ninja (https://ioninja.com).
+
+## Installation
+
+Register this tap with your Homebrew: 
+
+```
+$ brew tap vovkos/ioninja`
+```
+
+Install IO Ninja from this tap:
+
+```
+$ brew cask install ioninja`
+```
