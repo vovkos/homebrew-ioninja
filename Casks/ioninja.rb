edbd451e14d7942ee5fb1d0ef535a261cdf8a36f
Vladimir Gladkov (office-mac) <vovkos@gmail.com>
2020-04-21 13:19:11 +0800

    try to make brew test-bot happy

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -1,22 +1,23 @@
-cask 'ioninja' do
-  version '3.14.1'
-  sha256 'eb413bf20e94bea137f61b715c67f0197252729fab56dbe48e8155e83dfaa30b'
+cask "ioninja" do
+  version "3.14.1"
+  sha256 "eb413bf20e94bea137f61b715c67f0197252729fab56dbe48e8155e83dfaa30b"
 
+  # the official archive of all ioninja releases is currently stored on tibbo.com
   url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-a-mac.tar.xz"
-  name 'IO Ninja'
-  homepage 'https://ioninja.com/'
-
+  name "IO Ninja"
+  homepage "https://ioninja.com/"
   auto_updates true
 
-  depends_on macos: '>= :sierra'
+  depends_on macos => ">= :sierra"
 
   app "ioninja-#{version}-a-mac/IO Ninja.app"
+
   # binary "#{appdir}/IO Ninja.app/Contents/MacOS/ioninja"
 
-  zap trash: [
-               '~/.ioninja',
-               '~/Library/Caches/com.tibbo.ioninja',
-               '~/Library/Preferences/com.tibbo.ioninja.plist',
-               '~/Library/Saved Application State/com.tibbo.ioninja.savedState',
-             ]
+  zap trash => [
+    "~/.ioninja",
+    "~/Library/Caches/com.tibbo.ioninja",
+    "~/Library/Preferences/com.tibbo.ioninja.plist",
+    "~/Library/Saved Application State/com.tibbo.ioninja.savedState",
+  ]
 end
