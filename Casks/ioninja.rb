e177aea3c71977ba26c0687490c1dc46064ab49f
Vladimir Gladkov (office-mac) <vovkos@gmail.com>
2020-04-24 12:06:31 +0800

    release: ioninja-3.14.2

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -1,18 +1,18 @@
 cask "ioninja" do
-  version "3.14.1"
-  sha256 "eb413bf20e94bea137f61b715c67f0197252729fab56dbe48e8155e83dfaa30b"
+  version "3.14.2"
+  version_tag ""
+  sha256 "4d60119b817705f557741b46b887d6e12e7c18b2bc64b493c0618af473b421c4"
 
   # tibbo.com was verified as official when first introduced to the cask
-  url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-a-mac.tar.xz"
+  url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}#{version_tag}-mac.tar.xz"
   name "IO Ninja"
   homepage "https://ioninja.com/"
 
   auto_updates true
   depends_on macos: ">= :sierra"
 
-  app "ioninja-#{version}-a-mac/IO Ninja.app"
-
-  # binary "#{appdir}/IO Ninja.app/Contents/MacOS/ioninja"
+  app "ioninja-#{version}#{version_tag}-mac/IO Ninja.app"
+  binary "#{appdir}/IO Ninja.app/Contents/MacOS/ioninja"
 
   zap trash: [
     "~/.ioninja",
