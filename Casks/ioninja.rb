ae793514805fb1e9d1af612820e403f6b8fc774e
Vladimir Gladkov (office-mac) <vovkos@gmail.com>
2020-05-08 17:46:28 +0800

    removed version_tag (homebrew complains about unexpected method)

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -1,17 +1,16 @@
 cask "ioninja" do
   version "3.14.2"
-  version_tag ""
   sha256 "4d60119b817705f557741b46b887d6e12e7c18b2bc64b493c0618af473b421c4"
 
   # tibbo.com was verified as official when first introduced to the cask
-  url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}#{version_tag}-mac.tar.xz"
+  url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac.tar.xz"
   name "IO Ninja"
   homepage "https://ioninja.com/"
 
   auto_updates true
   depends_on macos: ">= :sierra"
 
-  app "ioninja-#{version}#{version_tag}-mac/IO Ninja.app"
+  app "ioninja-#{version}-mac/IO Ninja.app"
   binary "#{appdir}/IO Ninja.app/Contents/MacOS/ioninja"
 
   zap trash: [
