23baff75b4a4897454c48c27764b1be0f3d60247
Vladimir Gladkov (office-arch) <vovkos@gmail.com>
2020-10-30 16:00:18 +0800

    release: ioninja-3.15.0

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -1,6 +1,6 @@
 cask "ioninja" do
-  version "3.14.4"
-  sha256 "d49b655ba32f58984724526056d6907c443acbcccd21c6334218413c294452b6"
+  version "3.15.0"
+  sha256 "596853fd73443bca2dbf0320b01dfbde00f3e80874a20d7a63e17834ebbc982f"
 
   # tibbo.com was verified as official when first introduced to the cask
   url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac.tar.xz"
