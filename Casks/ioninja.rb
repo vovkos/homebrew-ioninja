3b9f0fa882d43df069bc6d6ad19f84f68e6ef538
Vladimir Gladkov (office-mac) <vovkos@gmail.com>
2021-09-17 14:40:27 +0800

    add the 'verified:' parameter as suggested in cask-cookbook

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -2,8 +2,8 @@ cask "ioninja" do
   version "5.0.3"
   sha256 "87cfff01c9bab335f5ff6ed21495bdf3fac726104fa903a3bd69a9e69f934a9d"
 
-  # tibbo.com was verified as official when first introduced to the cask
-  url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac.tar.xz"
+  url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac.tar.xz",
+    verified: "https://tibbo.com/downloads/archive/ioninja"
   name "IO Ninja"
   desc "Professional, scriptable, all-in-one terminal/sniffer"
   homepage "https://ioninja.com/"
