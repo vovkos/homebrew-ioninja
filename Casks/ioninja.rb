821583a4afa1b1fd30c0780e0e1ac47ac35dc988
Vladimir Gladkov (office-mac) <vovkos@gmail.com>
2022-09-16 20:47:22 +0800

    release: ioninja-5.2.0

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -1,6 +1,6 @@
 cask "ioninja" do
-  version "5.1.1"
-  sha256 "dff69425f5d38f93cb3147e902fe3ff2b59cbfff245ee28877a6f9ba6c166adc"
+  version "5.2.0"
+  sha256 "176bc0fa8774d3484a86d05c0eb3dfecdb79f2ef37c08694a59dceaf0cf5cfe4"
 
   url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac.tar.xz",
       verified: "https://tibbo.com/downloads/archive/ioninja"
