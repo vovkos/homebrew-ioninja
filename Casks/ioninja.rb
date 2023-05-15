02836433a90e5e76fdc5abe0bfb554410dfb6332
Vladimir Gladkov (office-mac) <vovkos@gmail.com>
2023-05-15 09:25:13 +0800

    fix the download link for ioninja-5.3.2

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -2,7 +2,7 @@ cask "ioninja" do
   version "5.3.2"
   sha256 "18773166d726dd77804928f4ea64b01bc419cb47e2f55c1540b3ac918c0cfaa0"
 
-  url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac.tar.xz",
+  url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac-amd64.tar.xz",
       verified: "https://tibbo.com/downloads/archive/ioninja"
   name "IO Ninja"
   desc "Professional, scriptable, all-in-one terminal/sniffer"
