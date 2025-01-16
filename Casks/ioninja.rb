384d7ec8bcbe663dec4de58198ec7690ddaebb8e
Vladimir Gladkov (office-mac) <vovkos@gmail.com>
2025-01-16 17:22:50 +0800

    release: ioninja-5.7.3

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -1,9 +1,9 @@
 cask "ioninja" do
-  version "5.7.2"
-  sha256 "a80b2b496e1f4274a2ba913bffc54ce7e499704b6f88223732e0686af1374e16"
+  version "5.7.3"
+  sha256 "70095023916bc083cbfc55ff64240b9924a05291f167315b32748b2dde990ead"
 
-  url "https://tibbo.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac-amd64.tar.xz",
-      verified: "https://tibbo.com/downloads/archive/ioninja"
+  url "https://ioninja.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac-amd64.tar.xz",
+      verified: "https://ioninja.com/downloads/archive/ioninja"
   name "IO Ninja"
   desc "Professional, scriptable, all-in-one terminal/sniffer"
   homepage "https://ioninja.com/"
