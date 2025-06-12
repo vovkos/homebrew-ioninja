f2d3383b15a08a0d9eedfe83878e22737db8c3f5
Vladimir Gladkov (office-ryzen-arch) <vovkos@gmail.com>
2025-06-12 18:56:54 +0800

    release: ioninja-5.7.5

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -1,6 +1,6 @@
 cask "ioninja" do
-  version "5.7.3"
-  sha256 "70095023916bc083cbfc55ff64240b9924a05291f167315b32748b2dde990ead"
+  version "5.7.5"
+  sha256 "26f0604dfe1c35373ce22fbb438d7a70167283e64116a6d56637b87deed2b227"
 
   url "https://ioninja.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac-amd64.tar.xz",
       verified: "https://ioninja.com/downloads/archive/ioninja"
