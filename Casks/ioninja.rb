e0b7b065cbdd610c00b7fe5d1ae314cf7992372d
Vladimir Gladkov (office-ryzen-arch) <vovkos@gmail.com>
2025-12-11 11:45:27 +0800

    release ioninja-5.9.1 plus github actions fixes to make brew test-bot happy

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -2,14 +2,12 @@ cask "ioninja" do
   version "5.9.1"
   sha256 "5b73a7dcdb45e1c6638163e11f884cde7124b86cfdf5e4f34a12e4ee1edd33c3"
 
-  url "https://ioninja.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac-amd64.tar.xz",
-      verified: "https://ioninja.com/downloads/archive/ioninja"
+  url "https://ioninja.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac-amd64.tar.xz"
   name "IO Ninja"
   desc "Professional, scriptable, all-in-one terminal/sniffer"
   homepage "https://ioninja.com/"
 
   auto_updates true
-  depends_on macos: ">= :sierra"
 
   app "ioninja-#{version}-mac-amd64/IO Ninja.app"
   binary "#{appdir}/IO Ninja.app/Contents/MacOS/ioninja"
