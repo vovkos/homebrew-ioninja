45bfc88b6c98021f0e149a3244263588b8e70385
Vladimir Gladkov (office-ryzen-arch) <vovkos@gmail.com>
2026-04-17 17:33:25 +0800

    release ioninja-5.10.2

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -1,15 +1,17 @@
 cask "ioninja" do
-  version "5.10.0"
-  sha256 "0645aba487c11b86b863871cf25522306e9e8ecca120b73d49352737fedec063"
+  version "5.10.2"
 
-  url "https://ioninja.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac-amd64.tar.xz"
+  arch intel: "amd64",
+       arm:   "arm64"
+
+  sha256 intel: "5c5241bee10c694e5ae49f3b005b771ea4d62845d5c67871495a7dc0152ad159",
+         arm:   "4f19305b3e38f2164f23d864fdd2ee704b182707032f8b421429928905f65d66"
+
+  url "https://ioninja.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac-#{arch}.tar.xz"
   name "IO Ninja"
   desc "Professional, scriptable, all-in-one terminal/sniffer"
   homepage "https://ioninja.com/"
-
-  auto_updates true
-
-  app "ioninja-#{version}-mac-amd64/IO Ninja.app"
+  app "ioninja-#{version}-mac-#{arch}/IO Ninja.app"
   binary "#{appdir}/IO Ninja.app/Contents/MacOS/ioninja"
 
   zap trash: [
