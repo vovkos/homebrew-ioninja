24aa26ba44d9fb0ba40577bc0d90aa195db7dfcc
Vladimir Gladkov (office-ryzen-arch) <vovkos@gmail.com>
2026-04-17 18:06:33 +0800

    fix: reorder stanzas, make test-bot happy

--- Casks/ioninja.rb
+++ Casks/ioninja.rb
@@ -1,16 +1,18 @@
 cask "ioninja" do
-  version "5.10.2"
-
   arch intel: "amd64",
        arm:   "arm64"
 
+  version "5.10.2"
+
   sha256 intel: "5c5241bee10c694e5ae49f3b005b771ea4d62845d5c67871495a7dc0152ad159",
          arm:   "4f19305b3e38f2164f23d864fdd2ee704b182707032f8b421429928905f65d66"
+
 
   url "https://ioninja.com/downloads/archive/ioninja/ioninja-#{version}/ioninja-#{version}-mac-#{arch}.tar.xz"
   name "IO Ninja"
   desc "Professional, scriptable, all-in-one terminal/sniffer"
   homepage "https://ioninja.com/"
+
   app "ioninja-#{version}-mac-#{arch}/IO Ninja.app"
   binary "#{appdir}/IO Ninja.app/Contents/MacOS/ioninja"
 
